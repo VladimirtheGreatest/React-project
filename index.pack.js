4628ba219f558ee00318570de85806c52874b76e
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-05-28 23:24:21 +0100

    Update index.pack.js

--- index.pack.js
+++ index.pack.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-// JSX
-ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
+import MyInfo from "./components/MyInfo"
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
