ae78c6137f5183d42d8b97e96e9ef91c2337a360
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-06-05 21:56:27 +0100

    Create Loggedincomponent.js

---
+++ components/Loggedincomponent.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+// Rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+    
+    render() {
+        let wordDisplay
+        if (this.state.isLoggedIn) {
+            wordDisplay = "in"
+        } else {
+            wordDisplay = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged {wordDisplay}</h1>
+            </div>
+        )
+    }
+}
+
+export default App
