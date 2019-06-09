f48206a16111d8cfd3884f154520837aa79a395b
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-06-09 09:06:25 +0100

    Update App.js
    Added changing state

--- components/App.js
+++ components/App.js
@@ -1,7 +1,3 @@
-/*
-Changing the function component to the class component, JSON data will be rendered from the state instead.
-*/
-
 import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
@@ -12,14 +8,30 @@ class App extends React.Component {
         this.state = {
             todos: todosData
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
     }
     
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
         
         return (
             <div className="todo-list">
                 {todoItems}
+                <button>Search</button>
             </div>
         )    
     }
