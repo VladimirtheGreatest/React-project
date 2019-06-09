73874c1bff6a9d1b8e1058bf0ae8b399a361136a
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-06-09 09:07:00 +0100

    Update TodoItem.js

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -3,9 +3,14 @@ import React from "react"
 function TodoItem(props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" />
-            <p>{props.x.text}</p>    //hardcoded html element will display paragraph with the text from the array 
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p>{props.item.text}</p>
         </div>
+        
     )
 }
 
